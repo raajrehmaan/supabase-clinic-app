bf5476d917948d0ef3ada7749ed5fcee68b39aac
raajrehmaan <raajrehmaan@gmail.com>
2026-06-05 11:16:45 +0100

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import ClinicApp from "./components/ClinicApp";
-
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-export default function Page() {
-  return <ClinicApp />;
-}
